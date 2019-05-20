47c73d80d1bf453c0d22f95d51c4fdeae90088ab
Остап П'ятковський <44294945+gestap@users.noreply.github.com>
2019-05-20 13:35:44 +0300

    need changes

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ let createTree = function(list, obj) {
 let textLine = function(obj) {
     let li = '';
     for (let key in obj) {
-        li += '<li>' + key + textLine(obj[key]) + '</li>'
+        li += '<li>' + key + textLine(obj[key]) + '</li>' // I would recommend use createElement instead Template strings
     }
-    if (li !== undefined) {
-        var ul = '<ul>' + li + '</ul>'
+    if (li !== undefined) { // In this case your create empty `ul`
+        var ul = '<ul>' + li + '</ul>' // use let or const and method createElement
     }
     return ul;
 }
 
-let list = document.getElementById('list');
+let list = document.getElementById('list'); // better to use `querySelector`
 createTree(list, data);
