7c96f5eb56286b5063457c9119216384087f7ca0
Antonenko Bohdan <bohdan1997@gmail.com>
2019-05-23 09:20:31 +0300

    changes

--- script.js
+++ script.js
@@ -22,14 +22,15 @@ let createTree = function(list, obj) {
 
 let textLine = function(obj) {
     let li = '';
+    let ul = '';
     for (let key in obj) {
         li += '<li>' + key + textLine(obj[key]) + '</li>'
     }
-    if (li !== undefined) {
-        var ul = '<ul>' + li + '</ul>'
+    if (li) {
+        ul = '<ul>' + li + '</ul>'
     }
     return ul;
 }
 
-let list = document.getElementById('list');
+let list = document.querySelector('#list');
 createTree(list, data);
